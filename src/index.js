e6c2005a6b385354c9b1036c008a09f66888da38
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-16 21:34:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import { h } from 'preact';
 import { render } from 'preact';
-import { Router } from 'wouter';
+import { Router, Route } from 'wouter';
 import Header from './components/Header';
-import Home from './pages/Home';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
 import './styles/main.scss';
+
+// Lazy load pages
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 const App = () => (
   <div>
     <Header />
     <Router>
       <main>
-        <Route path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route component={NotFound} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={NotFound} />
+        </Suspense>
       </main>
     </Router>
   </div>
